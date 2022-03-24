171088fc849b93a47e8b5265b14d05a3b357085c
daundkarash <47842758+daundkarash@users.noreply.github.com>
2022-03-24 16:30:42 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,22 +1,19 @@
 import streamlit as st
 from gsheetsdb import connect
+
 # Create a connection object.
 conn = connect()
+
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
 def run_query(query):
     rows = conn.execute(query, headers=1)
     return rows
-sheet1_url = st.secrets["gsheets"]["source_connectors"]
-rows = run_query(f'SELECT * FROM "{sheet1_url}"')
-st.subheader('Source Connectors:')
+
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
 # Print results.
 for row in rows:
-    st.write(f"{row.Description} has a :{row.Hevo}:")
-sheet2_url = st.secrets["gsheets"]["miscellaneous"]
-rows = run_query(f'SELECT * FROM "{sheet2_url}"')
-st.subheader('Miscellaneous:')
-# Print results.
-for row in rows:
-    st.write(f"{row.Description}")
+    st.write(f"{row.PlatformFeatures}")
